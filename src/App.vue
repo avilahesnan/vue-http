dffe14ad8726940f34d9c8f7f096e50b109d7afd
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-15 11:47:24 -0300

    feat: add components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="container container-fluid">
+      <div class="container">
+        <h1 class="display-4">Requisições HTTP no Vue</h1>
+        <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <TarefasLista />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TarefasLista from './components/TarefasLista.vue'
+
+export default {
+  components: {
+    TarefasLista
+  }
+}
+</script>
---
+++ src/components/TarefasListaItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <li class="list-group-item d-flex">
+        <span>{{ tarefa.titulo }}</span>
+        <span class="espacar"></span>
+        <button
+            class="btn btn-sm me-4"
+            :class="classeCSS"
+            :title="tituloBotaoConcluido">
+                <i class="fa-solid fa-check"></i>
+        </button>
+        <button
+            class="btn btn-primary btn-sm me-1"
+            title="Editar">
+                <i class="fa-solid fa-pencil"></i>
+        </button>
+        <button
+            class="btn btn-danger btn-sm"
+            title="Deletar">
+                <i class="fa-solid fa-trash"></i>
+        </button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        tarefa: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        classeCSS () {
+            return {
+                'btn-secondary': !this.tarefa.concluido,
+                'btn-success': this.tarefa.concluido
+            }
+        },
+        tituloBotaoConcluido () {
+            return this.tarefa.concluido
+                ? 'Refazer Tarefa'
+                : 'Concluir Tarefa'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .espacar {
+        flex: 1 1 auto;
+    }
+</style>
